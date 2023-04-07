1ec8946bef58170fb574035a78a5be1b1fe94ffe
Oluoma Eziolise <eziolisedivine@gmail.com>
2023-04-07 01:07:12 +0000

    updated hook and restart script

--- deployment/restart.sh
+++ deployment/restart.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+docker stop site
+docker rm site
 docker pull oeziolise/starter-website:latest
-docker run -p 8080:80 oeziolise/starter-website:latest
+docker run -p 8080:80 --name site oeziolise/starter-website:latest
