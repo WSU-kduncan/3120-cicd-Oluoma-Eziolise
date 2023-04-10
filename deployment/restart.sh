5c7edcb76f813cb3ec802a355f42f7110c426a3c
Oluoma Eziolise <eziolisedivine@gmail.com>
2023-04-10 14:18:36 +0000

    updated script run command

--- deployment/restart.sh
+++ deployment/restart.sh
@@ -2,4 +2,4 @@
 docker stop site
 docker rm site
 docker pull oeziolise/starter-website:latest
-docker run -p 8080:80 --name site oeziolise/starter-website:latest
+docker run -p 8080:80 --name site --restart=always oeziolise/starter-website:latest
